86635ab9d28103fc414a3378e38184e0890bbb32
hungdhv97 <hungdhv97@gmail.com>
2025-08-04 17:29:35 +0700

    Fix: Frontend blank screen issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,8 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Words from "@/pages/Words";
-import TodayReview from "@/pages/TodayReview";
-import History from "@/pages/History";
-
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/words" element={<Words />} />
-          <Route path="/today-review" element={<TodayReview />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <div>
+      <h1>Hello from React App!</h1>
+    </div>
   )
 }
 
