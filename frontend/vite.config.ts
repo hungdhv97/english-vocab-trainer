cdef768bbab7843ce25fe27e5af1987eb8cd6b7c
hungdhv97 <hungdhv97@gmail.com>
2025-08-05 15:08:01 +0700

    Second commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
