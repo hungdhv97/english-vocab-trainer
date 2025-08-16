bd2d09ff36fe42895bb7cb2e7fa50d5118c36cf9
hungdhv97 <hungdhv97@gmail.com>
2025-08-16 23:47:12 +0700

    Update

--- backend/migrations/data/0001_seed_minimal.down.sql
+++ backend/migrations/data/0001_seed_minimal.down.sql
@@ -1,3 +1,1 @@
-DELETE FROM words
-WHERE (language_code = 'en' AND word_text IN ('apple','banana','cat'))
-   OR (language_code = 'vi' AND word_text IN (E'táo', E'chuối', E'mèo'));
+DELETE FROM words;
